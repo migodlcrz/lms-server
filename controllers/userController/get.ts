6cc576d9137858bab0d050b0faa817594df08fa1
migodlcrz <lorenzo.cap.delacruz@gmail.com>
2024-06-27 23:09:15 +0800

    minor changes

--- controllers/userController/get.ts
+++ controllers/userController/get.ts
@@ -6,27 +6,35 @@ import mongoose from "mongoose";
 import jwt, { Secret } from "jsonwebtoken";
 
 export const getUsers = async (req: Request, res: Response) => {
-  const users = await User.find({}).sort({ createdAt: -1 });
+  try {
+    const users = await User.find({}).sort({ createdAt: -1 });
 
-  if (!users) {
-    res.status(400).json({ error: "No users found" });
+    if (!users || users.length === 0) {
+      return res.status(400).json({ error: "No users found" });
+    }
+
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
   }
-
-  res.status(200).json(users);
 };
 
 export const getUser = async (req: Request, res: Response) => {
   const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: "Course not found." });
+    return res.status(404).json({ error: "User not found." });
   }
 
-  const user = await User.findById(id).sort({ createdAt: -1 });
+  try {
+    const user = await User.findById(id);
 
-  if (!user) {
-    res.status(404).json({ error: "No users found" });
+    if (!user) {
+      return res.status(404).json({ error: "No user found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
   }
-
-  res.status(200).json(user);
 };

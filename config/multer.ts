19d6b892bd867e8ad6b4359e8ba30e16191a8b7f
migodlcrz <lorenzo.cap.delacruz@gmail.com>
2024-07-04 01:17:18 +0800

    added upload video endpoints

--- config/multer.ts
+++ config/multer.ts
@@ -1,17 +1,30 @@
+// multer.ts
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import cloudinary from "../cloudinary/cloudinary";
 
-const storage = new CloudinaryStorage({
+const imageStorage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: "uploads",
+    folder: "uploads/images",
     format: async (req: any, file: any) => "jpg",
     public_id: (req: any, file: { originalname: string }) =>
       file.originalname.split(".")[0],
   } as any,
 });
 
-const upload = multer({ storage: storage });
+const videoStorage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "uploads/videos",
+    resource_type: "video",
+    format: async (req: any, file: any) => "mp4",
+    public_id: (req: any, file: { originalname: string }) =>
+      file.originalname.split(".")[0],
+  } as any,
+});
 
-export default upload;
+const imageUpload = multer({ storage: imageStorage });
+const videoUpload = multer({ storage: videoStorage });
+
+export { imageUpload, videoUpload };
--- routes/module.ts
+++ routes/module.ts
@@ -3,8 +3,9 @@ import express from "express";
 import { createModule } from "../controllers/moduleController/create";
 import { getModules, getModule } from "../controllers/moduleController/get";
 import { uploadFile } from "../controllers/moduleController/uploadFile";
+import { uploadVideo } from "../controllers/moduleController/uploadVideo";
 import { deleteFile } from "../controllers/moduleController/deleteFile";
-import upload from "../config/multer";
+import { imageUpload, videoUpload } from "../config/multer";
 
 const mod = express.Router();
 
@@ -14,7 +15,8 @@ mod.get("/:id", getModule);
 
 mod.post("/create/:courseID", createModule);
 
-mod.post("/uploadFile/:moduleID", upload.single("picture"), uploadFile);
+mod.post("/uploadFile/:moduleID", imageUpload.single("picture"), uploadFile);
+mod.post("/uploadVideo/:moduleID", videoUpload.single("video"), uploadVideo);
 
 mod.delete("/deleteFile/:moduleID", deleteFile);
 
